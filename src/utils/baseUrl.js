f0153b0dcdfd65f95dff9126d09d4b73ce7cffa8
Emmanuel <twentekghana@gmail.com>
2021-08-30 18:19:26 +0800

    make it better

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,3 +1,3 @@
-const baseUrl = "http://localhost:5000";
-
+// const baseUrl = "http://localhost:5000";
+const baseUrl = "https://expenses-tracker-backend-v1.herokuapp.com";
 export default baseUrl;
